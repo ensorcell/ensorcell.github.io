c8fce3860ac984a62ca42b693f0ccafaf42e58e7
ensorcell <ensorcell@teknik.io>
2019-11-03 11:21:41 -0800

    ensorcraft

--- minecraft/overviewerConfig.js
+++ minecraft/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1572724254",
+        "cacheTag": "1572800169",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#36393f",
             "world": "ensorcraft",
-            "last_rendertime": 1572721446,
+            "last_rendertime": 1572800016,
             "imgextension": "jpg",
             "isOverlay": false,
             "poititle": "Markers",
@@ -72,7 +72,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#36393f",
             "world": "ensorcraft",
-            "last_rendertime": 1572721446,
+            "last_rendertime": 1572800016,
             "imgextension": "jpg",
             "isOverlay": false,
             "poititle": "Markers",
